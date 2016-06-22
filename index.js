06a3ad7f5ed0304004d648069a82e88a0afd27e6
Chen Zizhao <chinsh@sandi.co.jp>
2016-06-22 15:39:46 +0900

    version 2

--- index.js
+++ index.js
@@ -2,6 +2,39 @@ exports.printMsg = function() {
   console.log("This is a message from the demo package");
 }
 exports.calculate_chn_gb11643_1999 = function(id17) {
-  if(id17.length <> 17){retrun }
+  if(id17.length != 17){retrun }
   return 'X' ;
 }
+
+
+/**
+ * Escape special characters in the given string of html.
+ *
+ * @param  {String} html
+ * @return {String}
+ */
+module.exports = {
+  escape: function(html) {
+    return String(html)
+      .replace(/&/g, '&amp;')
+      .replace(/"/g, '&quot;')
+      .replace(/'/g, '&#39;')
+      .replace(/</g, '&lt;')
+      .replace(/>/g, '&gt;');
+  },
+
+  /**
+   * Unescape special characters in the given string of html.
+   *
+   * @param  {String} html
+   * @return {String}
+   */
+  unescape: function(html) {
+    return String(html)
+      .replace(/&amp;/g, '&')
+      .replace(/&quot;/g, '"')
+      .replace(/&#39;/g, "'")
+      .replace(/&lt;/g, '<')
+      .replace(/&gt;/g, '>');
+  }
+};
