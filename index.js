671fd3c34da662235809772260db8af2b7470f7d
CHEN,ZIZHAO <ncowboy@gmail.com>
2016-06-22 15:02:41 +0900

    first version

---
+++ index.js
@@ -0,0 +1,7 @@
+exports.printMsg = function() {
+  console.log("This is a message from the demo package");
+}
+exports.calculate_chn_gb11643_1999 = function(id17) {
+  if(id17.length <> 17){retrun }
+  return 'X' ;
+}
